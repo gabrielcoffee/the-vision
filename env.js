5405a80c4478f116252ec0c538c7bfb7b615755a
gabrielcoffee <cafegabriel1@gmail.com>
2025-04-10 21:32:55 -0300

    add: email public keys

---
+++ env.js
@@ -0,0 +1,6 @@
+// These are public EmailJS keys safe for frontend use
+window.ENV = {
+    EMAILJS_PUBLIC_KEY: "pqdr857dpI9_bbLZr",
+    SERVICE_ID: "service_eqyplbu",
+    TEMPLATE_ID: "template_xv3b88h"
+};  
